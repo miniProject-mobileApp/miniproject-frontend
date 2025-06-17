818bb73a6a5f535a607cd5ca3728383f00f4a56a
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-06-17 02:47:57 +0000

    completed ui for login page

--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -4,7 +4,15 @@ import 'package:flutter/services.dart';
 import 'package:frontend/screens/onboardingScreen.dart';
 
 
-class LoginScreen extends StatelessWidget{
+class LoginScreen extends StatefulWidget{
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isPasswordVisible = false; // handles visibility on the password text
+
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
@@ -96,6 +104,7 @@ class LoginScreen extends StatelessWidget{
                         borderRadius: BorderRadius.circular(35),
                         borderSide: BorderSide(color: Colors.blue, width: 2)
                       ),
+                      // labelText: "email",
                       hintText: "example@gmail.com",
                       contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
                       suffixIcon: Icon(Icons.email),
@@ -103,22 +112,161 @@ class LoginScreen extends StatelessWidget{
                     ),
                   ),
                 ),
-                
+                //password field
+                const SizedBox(height: 30),
+                const Text(
+                  "Password",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.blue
+                  ),
+                ),
+                //text input for password
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: screenWidth - 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder( // creates the border for the text input
+                        borderRadius: BorderRadius.circular(35)
+                      ),
+                      enabledBorder: OutlineInputBorder( // border style when not in focus
+                        borderRadius: BorderRadius.circular(35),
+                        borderSide:  BorderSide(color: Colors.grey, width: 1)
+                      ),
+                      focusedBorder: OutlineInputBorder( // border style when in focus
+                        borderRadius: BorderRadius.circular(35),
+                        borderSide: BorderSide(color: Colors.blue, width: 2)
+                      ),
+                      hintText: "enter your password here",
+                      contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isPasswordVisible = !isPasswordVisible;
+                          });
+                        }, 
+                        icon: Icon(
+                          isPasswordVisible ? Icons.visibility_off : Icons.visibility
+                        )
+                      ),
+                      suffixIconColor: Colors.blue,
+                    ),
+                    obscureText: !isPasswordVisible,
+                  ),
+                ),
+                //forgot password text
+                const SizedBox(height: 10,),
+                SizedBox(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end, // wrapped in row and used this alignment to place 
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          // logic for forgot password
+                        },
+                        child: const Text(
+                          "Forgot password?",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 14,
+                            decoration: TextDecoration.underline
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                //login button
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: screenWidth - 40,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      //logic for pressing login
+                    }, 
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32)
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                      elevation: 0
+                    ),
+                    child: const Text(
+                      "Log in",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16
+                      ),
+                    )
+                  ),
+                ),
+                const SizedBox(height: 20,),
 
-                
-                
+                // lines along side OR text
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 1,
+                        color: Colors.blue,
+                      )
+                    ),
+                    // padding between text and line on the right
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8)
+                    ),
+                    Text(
+                      "OR",
+                      style: TextStyle(color: Colors.blue, fontSize: 14),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 6)
+                    ),
+                    Expanded(
+                      child: Container(
+                      height: 1,
+                      color: Colors.blue,
+                      )
+                    )
+                  ],
+                ),
+                //sign in with google button
+                const SizedBox(height: 20,),
+                SizedBox(
+                  width: screenWidth - 40, // width of button
+                  height: 50, // height of button
+                  child: OutlinedButton(
+                    onPressed: (){
+                      //sign in with google logic
+                    }, 
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: Colors.blue, width: 2),
+                      
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Sign in with Google",
+                          style: TextStyle(color: Colors.blue, fontSize: 14),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 12)
+                        ),
+                        Image.asset("assets/images/google.png",
+                        )
+                      ],
+                    )
+                  )
+                )
               ],
               ),
-              
             )
-
-            
-  
           ],
         )
       ),
-      
     );
-
   }
 }
