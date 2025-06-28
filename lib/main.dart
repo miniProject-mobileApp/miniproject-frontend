954db504905d1847ab0da3557cf2b3de9f7c67c4
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-06-28 21:59:52 +0000

    updated home to signUpScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/basePage.dart';
+import 'package:frontend/screens/oldSignUpPage.dart';
+import 'package:frontend/screens/onboardingScreen.dart';
+import 'package:frontend/screens/signUpPage.dart';
 // import 'package:frontend/screens/homePage.dart';
 // import 'package:frontend/screens/lessonPage.dart';
 // import 'package:frontend/screens/onboardingScreen.dart';
@@ -18,7 +21,7 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.blue),
-      home: BaseScreen()
+      home: SignUpScreen()
     );
   }
 }
