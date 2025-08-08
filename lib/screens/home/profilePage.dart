73cd6a2f9c52d17282f5b7fabe5f9dea239160ae
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-08-08 23:22:20 +0000

    profile page completed

--- lib/screens/home/profilePage.dart
+++ lib/screens/home/profilePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:frontend/providers/auth_provider.dart';
+import 'package:provider/provider.dart';
 
 class ProfileScreen extends StatefulWidget{
   const ProfileScreen({super.key});
@@ -11,6 +13,9 @@ class ProfileScreen extends StatefulWidget{
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+    double screenWidth = MediaQuery.of(context).size.width;
+
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
@@ -64,137 +69,126 @@ class _ProfileScreenState extends State<ProfileScreen> {
               ),
           
               Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                    child: Text("Personal information",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.blue), ),
+                    padding: EdgeInsets.only(top: 10),
+                    child: Text("${authProvider.userName}",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.blue), ),
                   ),
                 ],
               ),
           
               // text fields
               Padding(
-                padding: EdgeInsets.symmetric(vertical: 50, horizontal: 16),
+                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 16),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    SizedBox(
-                      width: 350,
-                      height: 50,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          // filled: true, // this works with the fillColor to set the background color for the text input
-                          // fillColor: Colors.grey[100],
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15)
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
-                            borderSide: BorderSide(color: Colors.blue)
-                          ),
-                          labelText: "Username",
-                          labelStyle: TextStyle(color: Colors.blue)
+                    Container(
+                      width: screenWidth * 0.77,
+                      //height: screenHeight * 0.07,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.blue.shade200),
+                        borderRadius: BorderRadius.circular(18)
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text("Full name", style: TextStyle(color: Colors.blue.shade200),),
+                            Text("${authProvider.userName} ${authProvider.lastName}", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.blue),)
+                          ],
                         ),
-                        
                       ),
                     ),
-          
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            width: 350,
-                            height: 50,
-                            child: TextField(
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                  borderSide: BorderSide(color: Colors.blue)
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                  borderSide: BorderSide(color: Colors.blue, width: 1)
-                                ),
-                                labelText: "Email",
-                                labelStyle: TextStyle(color: Colors.blue)
-                              ),
-                            ),
-                          )
-                        ],
+
+                    SizedBox(height: 40,),
+
+                    Container(
+                      width: screenWidth * 0.77,
+                      //height: screenHeight * 0.07,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.blue.shade200),
+                        borderRadius: BorderRadius.circular(18)
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text("phone", style: TextStyle(color: Colors.blue.shade200),),
+                            Text("${authProvider.phoneNumber}", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.blue),)
+                          ],
+                        ),
                       ),
                     ),
 
                     //phone
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 4),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            width: 350,
-                            height: 50,
-                            child: TextField(
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                  borderSide: BorderSide(color: Colors.blue)
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                  borderSide: BorderSide(color: Colors.blue, width: 1)
-                                ),
-                                labelText: "Mobile phone",
-                                labelStyle: TextStyle(color: Colors.blue)
-                              ),
-                            ),
-                          )
-                        ],
+                    SizedBox(height: 40,),
+
+                    Container(
+                      width: screenWidth * 0.77,
+                      //height: screenHeight * 0.07,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.blue.shade200),
+                        borderRadius: BorderRadius.circular(18)
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text("email", style: TextStyle(color: Colors.blue.shade200),),
+                            Text("${authProvider.email}", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.blue),)
+                          ],
+                        ),
                       ),
                     ),
                   ],
                 ),
               ),
 
-              // security
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Text("Security", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue, fontSize: 18),),
-                  )
-                ],
-              ),
+              // // security
+              // Row(
+              //   children: [
+              //     Padding(
+              //       padding: const EdgeInsets.symmetric(horizontal: 16),
+              //       child: Text("Security", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue, fontSize: 18),),
+              //     )
+              //   ],
+              // ),
 
-              SizedBox(height: 30,),
+              // SizedBox(height: 30,),
 
-              // change password
-              Container(
-                width: 350,
-                height: 50,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1, color: Colors.blue),
-                  borderRadius: BorderRadius.circular(12),
-                  color: const Color.fromARGB(255, 225, 237, 247)
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: Text("Change you password", style: TextStyle(color: Colors.blue),),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 20),
-                      child: Icon(Icons.arrow_forward_ios, size: 20,),
-                    )
-                  ],
-                )
-              ),
+              // // change password
+              // Container(
+              //   width: 350,
+              //   height: 50,
+              //   decoration: BoxDecoration(
+              //     border: Border.all(width: 1, color: Colors.blue),
+              //     borderRadius: BorderRadius.circular(12),
+              //     color: const Color.fromARGB(255, 225, 237, 247)
+              //   ),
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //     children: [
+              //       Padding(
+              //         padding: const EdgeInsets.symmetric(horizontal: 16),
+              //         child: Text("Change you password", style: TextStyle(color: Colors.blue),),
+              //       ),
+              //       Padding(
+              //         padding: const EdgeInsets.only(right: 20),
+              //         child: Icon(Icons.arrow_forward_ios, size: 20,),
+              //       )
+              //     ],
+              //   )
+              // ),
 
-              SizedBox(height: 30,),
+              // SizedBox(height: 30,),
 
           
             ],
