ebf1f145bd8149203abf6b97f5623932a210ff91
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-08-14 22:27:23 +0000

    included video for investment lesson 3

--- lib/screens/courseDetails/Investment/Lessons/invest_lesson_three.dart
+++ lib/screens/courseDetails/Investment/Lessons/invest_lesson_three.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:frontend/screens/quiz/investment/question_one.dart';
 import 'package:frontend/screens/quiz/investment/question_three.dart';
 import 'package:video_player/video_player.dart';
 
